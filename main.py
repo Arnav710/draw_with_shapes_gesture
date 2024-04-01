cd66ba0dcd0a4561f1cb2e774a3aaa8c3ef93e56
Arnav Modi <mailto.arnav.modi@gmail.com>
2024-04-01 10:22:32 -0700

    change move rectangle logic

--- main.py
+++ main.py
@@ -2,10 +2,12 @@
 import cv2
 import mediapipe as mp
 import numpy as np
+import time
 
 # Import constants and functions
 import constants
 from button import draw_button
+from shapes import add_rectangle, draw_rectangles, move_rectangle
 
 # Initialize mediapipe hands module
 mphands = mp.solutions.hands
@@ -24,39 +26,13 @@ button_y = constants.BUTTON_TOP_RIGHT_Y
 button_width = constants.BUTTON_WIDTH
 button_height = constants.BUTTON_HEIGHT
 
-# Flag to indicate if the button was clicked
+# Flags
 button_clicked = False
+move_rectangle_enabled = False
+start_time = None
 
 # List to store rectangle coordinates
 rectangle_list = []
-
-def add_rectangle():
-    """
-    Add rectangle coordinates to the list with noise added to the top-right corner.
-
-    Parameters:
-        winwidth (int): Width of the window.
-    """
-    rect_size = (100, 100)
-    noise_range = (-10, 10)  # Range for noise addition
-    
-    # Generate noise for the top-right corner
-    noise_x = np.random.randint(noise_range[0], noise_range[1] + 1)
-    noise_y = np.random.randint(noise_range[0], noise_range[1] + 1)
-    
-    # Calculate the coordinates for the top-right corner with noise added
-    top_right_x = max(winwidth - rect_size[0] - 20 + noise_x, 0)
-    top_right_y = max(20 + noise_y, 0)
-    
-    top_right = (top_right_x, top_right_y)
-    
-    bottom_left = (top_right[0] + rect_size[0], top_right[1] + rect_size[1])
-    rectangle_list.append((top_right, bottom_left))
-
-def draw_rectangles(img):
-    # Draw all rectangles stored in the list onto the frame
-    for top_right, bottom_left in rectangle_list:
-        cv2.rectangle(img, top_right, bottom_left, (0, 255, 0), -1)
 
 def on_mouse(event, x, y, flags, param):
     global button_clicked
@@ -69,7 +45,7 @@ def on_mouse(event, x, y, flags, param):
             if not button_clicked:
                 button_clicked = True
 
-def index_finger_inside_rectangle(rec, x, y):
+def finger_inside_rectangle(rec, x, y):
     top_right, bottom_left = rec
 
     x_in_bounds = top_right[0] <= x and x <= bottom_left[0]
@@ -77,18 +53,6 @@ def index_finger_inside_rectangle(rec, x, y):
 
     return x_in_bounds and y_in_bounds
 
-def move_rectangle(x, y):
-    global rectangle_list
-
-    top_right, bottom_left = rectangle_list[-1]
-
-    rectangle_width = bottom_left[0] - top_right[0]
-    rectangle_height = bottom_left[1] - top_right[1]
-
-    new_top_right = (int(x - rectangle_width / 2), int(y - rectangle_height / 2))
-    new_bottom_left = (new_top_right[0] + rectangle_width, new_top_right[1] + rectangle_height)
-
-    rectangle_list[-1] = (new_top_right, new_bottom_left)
 
 # Set up the mouse callback
 cv2.namedWindow('Hand Tracking')
@@ -115,18 +79,39 @@ with mphands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5) as
             index_finger_landmark = None
             for lm in processFrames.multi_hand_landmarks:
                 mpdrawing.draw_landmarks(frame, lm, mphands.HAND_CONNECTIONS)
+                
                 index_finger_landmark = lm.landmark[8]
-                print(index_finger_landmark)
+                middle_finger_landmark = lm.landmark[12]
+
                 # Index finger position in pixel coordinates
                 index_finger_x = int(index_finger_landmark.x * winwidth)
                 index_finger_y = int(index_finger_landmark.y * winheight)
 
+                # Middle finger position in pixel coordinates
+                middle_finger_x = int(middle_finger_landmark.x * winwidth)
+                middle_finger_y = int(middle_finger_landmark.y * winheight)
+
                 # If the index finger lies inside the last rectangle placed, we can move it through gestures
                 if len(rectangle_list) > 0:
                     rec = rectangle_list[-1]
-                    if index_finger_inside_rectangle(rec, index_finger_x, index_finger_y):
+
+                    if finger_inside_rectangle(rec, index_finger_x, index_finger_y) \
+                        and finger_inside_rectangle(rec, middle_finger_x, middle_finger_y):
+                        if start_time == None:
+                            start_time = time.time()
+                        elif time.time() - start_time >= 2:
+                            print("move rectangle enabled")
+                            move_rectangle_enabled = True
+                        
+                    else:
+                        print("move rectangle disabled")
+                        start_time = None
+                        move_rectangle_enabled = False
+                    
+
+                    if move_rectangle_enabled:
                         # Move the most recently added rectangle
-                        move_rectangle(index_finger_x, index_finger_y)
+                        move_rectangle(index_finger_x, index_finger_y, rectangle_list)
 
         # Resize the frame to the desired window size
         resized_frame = cv2.resize(frame, (winwidth, winheight))
@@ -137,12 +122,12 @@ with mphands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5) as
         # If button is clicked, add rectangle coordinates to the list
         if button_clicked:
             print("Clicked")
-            add_rectangle()
+            add_rectangle(rectangle_list)
             button_clicked = False  # Reset the button clicked flag
 
 
         # Draw all rectangles onto the frame
-        draw_rectangles(resized_frame)
+        draw_rectangles(resized_frame, rectangle_list)
 
         # Display the resized frame
         cv2.imshow('Hand Tracking', resized_frame)
