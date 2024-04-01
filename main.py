cf22446230298569698a7d572f903785a5c5736f
Arnav Modi <mailto.arnav.modi@gmail.com>
2024-04-01 09:10:24 -0700

    remove draw_button from main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+# Import packages
 import cv2
 import mediapipe as mp
 import numpy as np
 
+# Import constants and functions
 import constants
+from button import draw_button
 
 # Initialize mediapipe hands module
 mphands = mp.solutions.hands
@@ -26,29 +29,6 @@ button_clicked = False
 
 # List to store rectangle coordinates
 rectangle_list = []
-
-def draw_button(img):
-    # Draw white filled rectangle with black border
-    cv2.rectangle(img, (button_x, button_y), (button_x + button_width, button_y + button_height), (255, 255, 255), -1)
-    cv2.rectangle(img, (button_x, button_y), (button_x + button_width, button_y + button_height), (0, 0, 0), 2)
-
-    # Add text inside the button
-    text = "Add Rectangle"
-    font = cv2.FONT_HERSHEY_SIMPLEX
-
-    # Adjust font size and thickness
-    font_scale = 0.8
-    thickness = 2
-
-    # Get text size and position
-    text_size = cv2.getTextSize(text, font, font_scale, thickness)[0]
-    text_x = button_x + (button_width - text_size[0]) // 2
-    text_y = button_y + (button_height + text_size[1]) // 2
-
-    # Draw text with increased spacing between letters
-    cv2.putText(img, text, (text_x, text_y), font, font_scale, (0, 0, 0), thickness, cv2.LINE_AA, False)
-
-    return img
 
 def add_rectangle():
     """
